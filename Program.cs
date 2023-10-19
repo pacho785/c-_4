5d2e518e88c6a11e8151ed2b7792f3001b3c530a
pacho785 <125918271+pacho785@users.noreply.github.com>
2023-10-19 16:58:16 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var (valor1,valor2, valor3) = (10, 10, 2);
+Console.WriteLine("los valores son: " + valor1 +", " + valor2 + ", " + valor3);
+
+//== igual a
+//!= diferente a 
+//> mayor que
+//< menor que
+//>= mayor a igual
+//<= menor o igual 
+
+bool resultado = valor1 == valor2;
+Console.WriteLine("El resultado de la comparacion es: " + resultado);
+
+bool rsultado2  = valor2 != valor2;
+Console.WriteLine("El resultado de la comparacion es: " + rsultado2 );
+
+bool resultado3 = valor1 > valor2;
+Console.WriteLine("El resultado de la comparacion es : " + resultado3);
+
+bool resultado4 =  valor1 < valor2;
+Console.WriteLine("El resultado de la comparacion es: " + resultado4);
+
+bool resultado5 = valor1 >= valor2;
+Console.WriteLine("El resultado de la comparacion es: " + resultado5);
+
+bool resultado6 = valor1 <= valor2;
+Console.WriteLine("El resultado de la comparcion es: " + resultado6);
